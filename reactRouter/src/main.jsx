a754f79f01e2ac261bf2073e0e7fd6b36a407182
Muhammad Hasan <hasanamir8901@gmail.com>
2026-02-19 00:19:06 +0500

    add router and page layouts

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,9 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github from "./components/Github/Github";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "user/:userid",
+        element: <User />,
+      },
+      {
+        path: "github",
+        element: <Github/>
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
