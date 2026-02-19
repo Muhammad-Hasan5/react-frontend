f54154f68760ea84fff1b7f1739ee8f786a59c13
Muhammad Hasan <hasanamir8901@gmail.com>
2026-02-20 03:38:57 +0500

    complete the todo app using react context api with local storage

--- contextLocalStorage/src/context/TodoContext.js
+++ contextLocalStorage/src/context/TodoContext.js
@@ -1,11 +1,22 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext();
+export const TodoContext = createContext({
+  todo: [
+    {
+      id: 1,
+      todo: "",
+      completed: false,
+    },
+  ],
+  addTodo: () => {},
+  deleteTodo: () => {},
+  updateTodo: () => {},
+  toggleComplete: () => {},
+});
 
 export const useTodo = () => {
   return useContext(TodoContext);
 };
 
-export const TodoContextProvider = TodoContext.Provider
-
+export const TodoContextProvider = TodoContext.Provider;
 
