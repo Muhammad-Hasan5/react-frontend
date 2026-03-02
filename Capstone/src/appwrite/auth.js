d8da8e7bce80d93546791736036b53cd7ed9be17
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-02 10:14:25 +0500

    add authentication for the app

---
+++ Capstone/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectID);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password }) {
+    try {
+      const user = await this.account.create({
+        userId: ID.unique(),
+        email,
+        password,
+      });
+
+      if (user) {
+        this.login({ email, password });
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession({ email, password });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async logout(){
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        throw new Error(error)
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
