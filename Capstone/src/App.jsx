286bbead0e35af8e760e193fa506539ed560d059
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-02 12:07:20 +0500

    create the webpage structure

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -1,12 +1,40 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice";
+import {Header, Footer} from "./components/index"
 import "./App.css";
 
 function App() {
-  console.log();
-  
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? (
     <>
-      <h1>A blog with Appwrite</h1>
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            TODO: {/*<Outlet />*/}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
+  ) : (
+    <></>
   );
 }
 
