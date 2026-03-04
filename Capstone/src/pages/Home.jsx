2aa1aed6199a9e81b1ec4ede69e6e1039ee1b223
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-04 09:16:52 +0500

    add home page

---
+++ Capstone/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import service from "../appwrite/config";
+import { Container, PostCard } from "../components";
+
+function Home() {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    service.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts);
+      }
+    });
+  });
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="w-full p-2">
+              <h1 className="text-2xl font-bold hover:text-gray-400">
+                Login to raed Posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts?.map((post) => {
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
+            </div>;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
