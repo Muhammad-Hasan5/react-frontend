f35ae1d1f3b136833334d44e17af95082f5aa682
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-04 09:13:12 +0500

    add authlayout

---
+++ Capstone/src/components/AuthLayout.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const [loader, setLoader] = useState(false);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : { children };
+}
