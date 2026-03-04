85847138e4cc4b5e5e1829e7250f4b3ef8572b53
Muhammad Hasan <hasanamir8901@gmail.com>
2026-03-04 10:40:02 +0500

    remove errors

--- Capstone/src/components/Header/LogoutBtn.jsx
+++ Capstone/src/components/Header/LogoutBtn.jsx
@@ -8,7 +8,7 @@ function LogoutBtn() {
     authService.logout().then(() => dispatch(logout()));
   };
   return (
-    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={() => logoutHandlerfce}>
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}>
       logout
     </button>
   );
--- Capstone/src/conf/conf.js
+++ Capstone/src/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
   appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteTableID: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
